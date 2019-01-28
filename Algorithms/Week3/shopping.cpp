c95adce9ab365ced054606cd42836923f00e350d
bmdoil <bmdoil1886@gmail.com>
2019-01-27 23:45:39 -0500

    remove vector

--- Algorithms/Week3/shopping.cpp
+++ Algorithms/Week3/shopping.cpp
@@ -5,20 +5,18 @@
 
 using namespace std;
 
-int shoppingCost(int*, int*, int, int, vector<int>&);
+int shoppingCost(int*, int*, int, int);
+int familyMemberMax(int*, int*, int*, int, int);
 
 int main(int argc, char* argv[]){
 
      int testCases;     
      int numItems;          
 
-     int prices[100];     
-     int weights[100];     
+         
 
      int familySize;          
-     int maxWeight = 0;   
-
-     vector<vector<int> > itemList(100);    
+     
 
      ifstream in;
      ofstream out;
@@ -43,110 +41,77 @@ int main(int argc, char* argv[]){
 
      for (int k = 0; k < testCases; k++){
 
-          // read the number of items 
+        // read the number of items 
+        in >> numItems;
+        int prices[numItems];     
+        int weights[numItems];
 
-          in >> numItems;
+        // read the price and weight of each item into arrays
 
-          // read the price and weight of each item into arrays
+        for (int i = 0; i < numItems; i++){
 
-          for (int i = 0; i < numItems; i++){
-
-              in >> prices[i];
-              in >> weights[i];
-          }
-
-          int maxPrice = 0;
+            in >> prices[i];
+            in >> weights[i];
+        }        
           
-          // read number of family members
-          in >> familySize;
+        // read number of family members
+        in >> familySize;
+        int max[familySize];
 
-          // find maximum price of items that can be carried by each family member and keep track the total of the maximum prices
+        // find maximum price of items that can be carried by each family member and keep track the total of the maximum prices
 
-          for (int j = 0; j < familySize; j++){
+        for (int j = 0; j < familySize; j++){
 
-              // read the maximum weight that can be carried by current family member
-              in >> maxWeight;
+            // read the maximum weight that can be carried by current family member
+              in >> max[j];
 
               // find maximum price of items that can be carried bycurrent family member and add it to maxPrice
 
-              maxPrice = maxPrice + shoppingCost(weights, prices, numItems, maxWeight, itemList[j]);
+              out << familyMemberMax(prices,weights,max,numItems,familySize) << endl;
 
+          
           }
-          // Write the maximum total price to out
-          out << "Test Case " << k << endl << "Total Price " << maxPrice << endl;    
-          out << "Member Items" << endl;
-
-          //Print items 
-
-          for (int f = 0; f < familySize; f++) {
-              //Sort items for this family member
-              sort(itemList[f].begin(), itemList[f].end());
-              out << f + 1 << ":";
-              for (int g = 0; g < itemList.size(); g++)
-              {
-                  out << itemList[f][g] << " ";
-              }
-              out << endl;
-          }
-
-
      }
-
 
      in.close();
      out.close();
 
      return 0;
-
 }
 
 // Returns the maximum price of items that can be carried by a person, given item weights, prices, number of items, and max weight person can carry
-int shoppingCost(int weights[], int prices[], int numItems, int maxWeight, vector<int> &items)
-
-{
+int shoppingCost(int weights[], int prices[], int numItems, int maxWeight){
 
      int result[numItems + 1][maxWeight + 1];
-
      // Build result table
+     for (int i = 0; i <= numItems; i++){
 
-     for (int i = 0; i <= numItems; i++)
-
-     {
-
-          for (int j = 0; j <= maxWeight; j++)
-
-          {
+          for (int j = 0; j <= maxWeight; j++){
               //Base case
               if (i == 0 || j == 0)
 
                    result[i][j] = 0;
 
               else if (weights[i - 1] <= j)
-
                    result[i][j] = max(prices[i - 1] + result[i - 1][j - weights[i - 1]], result[i - 1][j]);
-
               else
-
                    result[i][j] = result[i - 1][j];
-
           }
-
      }
-
     // result[numItems][maxWeight] represents the maximum price of items that can be carried by the family member
-    int priceMax = result[numItems][maxWeight];
-    int w = maxWeight;
+    return result[numItems][maxWeight];
+}
 
-    //Add to items vector
-    for (int i = numItems; i > 0 && priceMax > 0; i--){
-        if (priceMax == result[i - 1][w]) continue;
-        else {
-            items.push_back(i);
-            priceMax = priceMax - prices[i - 1];
-            w = w - weights[i - 1];
-        }
+
+int familyMemberMax(int prices[], int weights[], int max[], int numItems, int family);
+{
+   
+    int totalPrice = 0;
+    
+    for (int i = 0; i < family; i++)
+    {
+        // Retrieve max price for current family member
+        totalPrice += shoppingCost(price, weight, max[i], number);
     }
-
-    return priceMax;
-
+    return totalPrice;
 }
