c192a68f8d33c9cd30a3433a66e306229fc2ae2c
Brent Doil <bmdoil1886@gmail.com>
2019-02-05 17:24:43 -0500

    Added selectSort, templating

--- Algorithms/Week1/algorithms.cpp
+++ Algorithms/Week1/algorithms.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <chrono>
 
+#define NUM_MAX 10000
+#define DEF_ITER 5
 using namespace std;
+using namespace std::chrono;
 
-void insertSort(int A[], int len) {
+void mergeSort(int* arr, int len);
+
+void insertSort(int* A, int len) {
     //create indexes
     int i, j, key;
 
@@ -20,11 +28,26 @@ void insertSort(int A[], int len) {
 }
 
 
-bool linearSearch(int A[], int len, int val) {
+bool linearSearch(int* A, int len, int val) {
     for (int i = 0; i < len; i++) {
         if (A[i] == val) return true;
     }
     return false;
+}
+
+bool binarySearch(int* A, int len, int val) {
+    //sort array using mergeSort
+    mergeSort(A,len);
+    int L = 0;
+    int R = len - 1;
+    int mid;
+    while (L < R){
+        mid = std::ceil( (L+R)/2 );
+        if (L == R) return true;
+        if (A[mid] < val) L = mid + 1;
+        else if (A[mid] > val) R = mid;        
+    }
+    return false;    
 }
 
 //Returns pointer to the new array
@@ -39,4 +62,90 @@ int* addBinary(int A[], int B[], int n) {
     }
     C[i] = carry;
     return C; 
+}
+
+void selectSort(int* A, int len) {
+    int min, j, i, tmp;
+    for (j = 0; j < len - 1; j++) {
+        min = j;
+        for (i = j + 1; i < len; i++) {
+            if (A[i] < A[min]) min = i;
+        }
+        tmp = A[min];
+        A[min] = A[j];
+        A[j] = tmp;
+    }
+}
+void merge(int* arr, int mid, int len) {
+
+    int i = 0;
+    int j = mid;
+    int k = 0;
+    int tmp[len];
+
+    while (i < mid && j < len) {
+        tmp[k++] = (arr[i] < arr[j]) ? arr[i++] : arr[j++];
+    }
+    memcpy(tmp+k, arr+i, (mid - i) * sizeof *arr);
+    memcpy(arr, tmp, (k + (mid - i)) * sizeof *arr);
+
+}
+void mergeSort(int* arr, int len) { 
+    int mid;
+    if (len < 2) return; //base case
+    mid = len / 2; //mid index
+    mergeSort(arr, mid);
+    mergeSort(arr + mid, len - mid);
+    merge(arr, mid, len);
+}
+
+int* randArr(int arrLen) {
+    int* array = new int[arrLen];
+    for (int j = 0; j < arrLen; j++) array[j] = rand() % NUM_MAX;
+    return array;
+}
+
+template<typename Function>
+void printSortTiming(int* A, int len, Function func) {    
+    
+    auto startTime = high_resolution_clock::now();
+    func(A, len);
+    auto stopTime = high_resolution_clock::now();
+    auto timeElapsed = duration_cast<milliseconds>(stopTime - startTime);
+    cout << len << "\t" << timeElapsed.count()<< endl;
+}
+
+template<typename Function>
+void testTiming(int startSize, int iterations, Function sortFunc, string funcName) {
+
+    for (int i = 1; i <= iterations; i++) {
+        int arrLen = startSize * i;
+        int* array = randArr(arrLen);
+        if (i == 1) cout << funcName << " Timing\nn\tms\n";
+        printSortTiming(array, arrLen, sortFunc);
+        delete [] array;
+    }
+}
+
+void printarr(int A[], int len) {
+    for (int i = 0; i < len; i++) cout << A[i] << " ";
+    cout << endl;
+}
+int main(int argc, char* argv[]) {
+
+    int startSize, iterations;
+
+    if (argc != 3) {
+        cout << "Usage: algTest [startSize] [iterations]" << endl;
+        return 1;
+    }    
+    
+    startSize = atoi(argv[1]);        
+    iterations = atoi(argv[2]);     
+    
+    testTiming(startSize, iterations, selectSort, "Select");
+    testTiming(startSize, iterations, insertSort, "Insert");
+    testTiming(startSize, iterations, mergeSort, "Merge");
+
+    return 0;    
 }
